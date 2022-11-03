cc4456aa6b2492dc8711cc8ab29e2f4d03ab0a56
DEAD POOL <78198419+not-DEADPOOL@users.noreply.github.com>
2022-11-03 12:44:39 +0100

    Add files via upload

---
+++ oop_3/oop_3/CameraCaffe.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_3
+{
+    class CameraCaffe
+    {
+        public int serbatoio = 1000;
+        public int Ncaffe = 0;
+        public bool tGrande;
+
+        public void Prepara(bool Grande)
+        {
+            if (serbatoio - 50 < 0 || serbatoio - 80 < 0)
+            {
+                Console.WriteLine(@"░░░░░▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄░░░░░░░
+░░░░░█░░░░▒▒▒▒▒▒▒▒▒▒▒▒░░▀▀▄░░░░
+░░░░█░░░▒▒▒▒▒▒░░░░░░░░▒▒▒░░█░░░
+░░░█░░░░░░▄██▀▄▄░░░░░▄▄▄░░░░█░░
+░▄▀▒▄▄▄▒░█▀▀▀▀▄▄█░░░██▄▄█░░░░█░
+█░▒█▒▄░▀▄▄▄▀░░░░░░░░█░░░▒▒▒▒▒░█
+█░▒█░█▀▄▄░░░░░█▀░░░░▀▄░░▄▀▀▀▄▒█
+░█░▀▄░█▄░█▀▄▄░▀░▀▀░▄▄▀░░░░█░░█░
+░░█░░░▀▄▀█▄▄░█▀▀▀▄▄▄▄▀▀█▀██░█░░
+░░░█░░░░██░░▀█▄▄▄█▄▄█▄████░█░░░
+░░░░█░░░░▀▀▄░█░░░█░█▀██████░█░░
+░░░░░▀▄░░░░░▀▀▄▄▄█▄█▄█▄█▄▀░░█░░
+░░░░░░░▀▄▄░▒▒▒▒░░░░░░░░░░▒░░░█░
+░░░░░░░░░░▀▀▄▄░▒▒▒▒▒▒▒▒▒▒░░░░█░
+░░░░░░░░░░░░░░▀▄▄▄▄▄░░░░░░░░█░░");
+                return;
+            }
+                serbatoio -= Grande ? 80 : 50;
+                Ncaffe++;
+            Console.WriteLine(@"Pronto!
+
+                            (  )   (   )  )
+                             ) (   )  (  (
+                             ( )  (    ) )
+                             _____________
+                            <_____________> ___
+                            |             |/ _ \
+                            |               | | |
+                            |               |_| |
+                         ___|             |\___/
+                        /    \___________/    \
+                        \_____________________/
+            ");
+        }
+        public void Riempi()
+        {
+            serbatoio = 1000;
+            Console.WriteLine("Riempito!");
+        }
+        public void Stato()
+        {
+            Console.WriteLine($"Il serbatoio contiene {serbatoio} di Acqua.");
+            Console.WriteLine($"Finora ho preparato {Ncaffe} caffe'");
+        }
+    }
+}
---
+++ oop_3/oop_3/caffe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_3
+{
+    class caffe
+    {
+        static void Main(string[] args)
+        {
+            int COMANDI = 1;
+            CameraCaffe caffe = new CameraCaffe();
+            Console.WriteLine("Hai a disposizione 5 comandi. :)");
+            do
+            {
+                Console.Write("inserire comando della macchina del caffè:");
+                char scelta = Convert.ToChar(Console.ReadLine());
+                if (scelta == 'p')
+                {
+                    Console.Write("Vuoi la tazza grande?[0/1]");
+                    char Grande =Convert.ToChar(Console.ReadLine());
+                    bool isGrande = Grande == 's' ? true : false;
+                    caffe.Prepara(isGrande);
+                }else if(scelta == 'r')
+                {
+                    caffe.Riempi();
+                }else if(scelta == 's')
+                {
+                    caffe.Stato();
+                }
+                else if (scelta == 'c')
+                {
+                    Console.Clear();
+                }
+
+                COMANDI++;
+            }while(COMANDI!=5);
+            Console.ReadKey();
+        }
+    }
+}
